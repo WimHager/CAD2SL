a072239e58665ba50ea9e5f7e8867ad17bb546b1
WimHager <wimhager@gmail.com>
2010-03-09 04:24:10 -0700

    Added more code for multi prim

--- v0/php/cad2sl.php
+++ v0/php/cad2sl.php
@@ -1,32 +1,6 @@
 <?
 
 //todo: filter on SL servers only
-//filter on name owner
-//remove not used (old) entry's
-
-function write_log($filename, $message) {
-    $fp = fopen($filename, "a+");
-    fwrite($fp, date("[Y/m/d-H:i:s];").$message."\n");
-    fclose($fp);
-}
-
-function load_data($filename) {
-    $content= file_get_contents($filename);
-    explode("/n", $content);
-    return($content);
-}
-
-function save_data($filename, $data) {
-    file_put_contents($filename, $data);
-}
-
-function request_headers() { // Replacement if apache_request_headers not exist 
-    foreach($_SERVER as $name => $value)
-    if(substr($name, 0, 5) == 'HTTP_')
-        $headers[str_replace('X-Secondlife-', 'X-SecondLife-', str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))))] = $value;
-    return $headers;
-}
-
 
 // Only works with PHP compiled as an Apache module
 //$headers = apache_request_headers();
@@ -39,31 +13,44 @@ $ownerName  = $headers["X-SecondLife-Owner-Name"];
 $region     = $headers["X-SecondLife-Region"];
 
 // get things from $_POST[]
-$name	= $_POST["name"];
-$func	= $_POST["func"];
-$cryp	= $_POST["cryp"];
-$primnr	= $_POST["primnr"];
-$data	= $_POST["data"];
+$name	= $_POST["name"]; 	//file name
+$primnr	= $_POST["primnr"];	//prim number we want to read
+$data	= $_POST["data"];	//prim data
 
-if (strlen($func) < 3) {
-	echo "Wrong SHA1 KEY, Action logged !!"; 
-	exit;
+//if (strlen($func) < 3) {
+//	echo "Wrong SHA1 KEY, Action logged !!"; 
+//	exit;
+//}
+
+$data= load_data($name);
+$hash= md5($data[$i].':'.$nonce);
+echo "&md5=".$hash."&data=".$data[$primnr]."&total=".count($data); //Don't change order !!!
+
+
+// Functions block =============================================================================
+
+function write_log($filename, $message) {
+    $fp = fopen($filename, "a+");
+    fwrite($fp, date("[Y/m/d-H:i:s];").$message."\n");
+    fclose($fp);
 }
 
-
-$nonce = 0;
-$hash = md5($data.':'.$nonce);
-
-if ($func == "PUT") {
-	if ($cryp == $hash) {
-		save_data($name,$data);
-		echo "Data ok.";
-	}else{ echo "Received Corrupt Data!!"; }
+function load_data($filename) {
+    $content= file_get_contents($filename);
+    $content= trim($content, "\n");	
+    $content= explode("\n", $content);
+    return($content);
 }
-if ($func == "GET") {
-	$data = load_data($name);
-	$hash = md5($data.':'.$nonce);
-	echo "&cryp=".$hash."&data=".$data; //Don't change order !!!
+
+function save_data($filename, $data) {
+    file_put_contents($filename, $data);
+}
+
+function request_headers() { // Replacement if apache_request_headers not exist 
+    foreach($_SERVER as $name => $value)
+    if(substr($name, 0, 5) == 'HTTP_')
+        $headers[str_replace('X-Secondlife-', 'X-SecondLife-', str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5))))))] = $value;
+    return $headers;
 }
 
 
