a36474e7e262360b545c87cf019d09e8f4bbc24c
tuxed <tuxednoel@gmail.com>
2010-03-11 12:45:37 +0100

    Updated cad2sl.php

--- v0/php/cad2sl.php
+++ v0/php/cad2sl.php
@@ -1,4 +1,6 @@
-<?php
+<h3>x3d upload interface for CAD2SL</h3>
+
+<?php 
 //Edit this part to your needs========================================== 
  
 $location=  "uploads/";			//Upload directory, must have write rights
@@ -9,43 +11,51 @@ $fileperm=  0444;
  
 //End of part===========================================================
  
-if(isset($_POST['upload']))
+if(isset($_POST['upload'])) 
 {
-    if(is_uploaded_file($_FILES['file']['tmp_name']))
-    {
-        $extention_file = pathinfo($_FILES['file']['name']);
-        $extention_file = $extention_file[extension];
- 
-        $extentions_allowed = explode(", ", $allowed);
- 
-        $ok = in_array($extention_file, $allowed);
- 
-        if($ok == 1)
-        {
-            if($_FILES['file']['size'] > $max_size)
-            {
-                echo "File is too big, Max. file size is: <b>".$max_size."</b>";
-                exit;
-            }
- 
-            if(!move_uploaded_file($_FILES['file']['tmp_name'],$location.$_FILES['file']['name']))
-            {
-                echo "File cannot be placed";
-                exit;
-            }
- 	    chmod($location . $_FILES['file']['name'], $fileperm);
-            echo "File ".$_FILES['file']['name']." is uploaded<br /><a href='".$location.$_FILES['file']['name']."' target='_blank'>click here to view the file</a><br />The link is : ". $youresite . $location .$_FILES['file']['name'];
-        }
-        else
-        {
-            echo "Wrong extention, allowd extentions are: <b>".$allowed[0].' '.$allowed[1]."</b>";
-        }
-    }
-    else
-    {
-        echo "Upload has failed!!!";
-    }
- 
+	if(is_uploaded_file($_FILES['file']['tmp_name'])) 
+	{
+		$extention_file = pathinfo($_FILES['file']['name']);
+        	$extention_file = $extention_file[extension];
+
+ 		$extentions_allowed = explode(", ", $allowed);
+ 	
+		$ok = in_array($extention_file, $allowed);
+ 	
+		if($ok == 1)
+		{		
+			if($_FILES['file']['size'] > $max_size)
+			{
+				echo "File is too big, Max. file size is: <b>".$max_size."</b>";
+				exit;
+			}
+		
+			if(!move_uploaded_file($_FILES['file']['tmp_name'],$location.$_FILES['file']['name'])) 
+			{
+				echo "File cannot be placed";
+				exit;
+			}
+
+			chmod($location . $_FILES['file']['name'], $fileperm);
+			echo "File: ".$_FILES['file']['name']." is uploaded<br />";				
+		}
+		else
+		{
+			echo "Wrong extention, allowd extentions are:<b>";
+
+			for ( $i = 0; $i < count($allowed); $i ++) 
+			{
+				echo " ." . $allowed[$i];
+			}
+			
+			echo "</b>";
+		}
+	}
+
+	else
+	{
+		echo "Upload has failed!!!";
+	}
 }
 ?>
 <br />
@@ -53,4 +63,3 @@ if(isset($_POST['upload']))
 <input type="file" name="file" /><br />
 <input type="submit" name="upload" value="Uploaden!" />
 </form>
-
