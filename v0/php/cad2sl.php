b19610ee62108c5c3daf10e083bc97157f754eb5
tuxed <tuxednoel@gmail.com>
2010-03-17 11:25:34 +0100

    cad2sl-conv.php is now build in cad2sl.php after uploading a .x3d file it auto convert the file to a .prim file

--- v0/php/cad2sl.php
+++ v0/php/cad2sl.php
@@ -2,9 +2,25 @@
 
 <?php 
 
+/*
+    This file is part of CAD2SL.
+
+    CAD2SL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CAD2SL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CAD2SL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 include 'cad2sl-const.php';
 include 'cad2sl-lib.php';
-include 'cad2sl-conv.php';
 
 if(isset($_POST['upload'])) 
 {
@@ -29,7 +45,15 @@ if(isset($_POST['upload']))
 			}
 
 			chmod($Location . $_FILES['file']['name'], $FilePerm);
-			echo "File: ".$_FILES['file']['name']." is uploaded<br />";				
+
+			//Start to convert the file and write a converted file.
+			$InF = 	$Location.$_FILES['file']['name'];
+			$OutF =	preg_replace('/\..+$/', '.' . $GLOBALS['OutputExt'], $InF);
+
+			WriteData(ConvInputFileToOutputStr($InF), $OutF);
+
+			echo "File: ".$Location.$_FILES['file']['name']." is uploaded & converted.<br />";
+		
 		}else{
 			echo "Wrong extention, allowd extentions are:<b>";
 
